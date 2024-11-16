2e5806bbf2469ae1a28833b4fc45a5980590b7e4
Patrick McDavid <fusionjunky@gmail.com>
2024-11-16 11:36:28 -0700

    game create endpoint, needs work

--- app.js
+++ app.js
@@ -43,6 +43,80 @@ app.get('/myTeams', verifyToken, async (req, res) => {
   res.send(guilds)
 })
 
+app.post('/games/:teamId/new', verifyToken, async (req, res) => {
+  const player = await infightDB.Player.findByPk(req.user.id)
+  if (player === null) {
+    res.send(new Error("User not logged in", { statusCode: 404 }))
+    next()
+  }
+
+  //check if we got a good id
+  if (!req.params.teamId) {
+    res.send(new Error("Invalid teamId", { statusCode: 404 }))
+    next()
+  }
+
+  // check if there's an active game
+  const g = await infightDB.Guild.findByPk(req.params.teamId);
+  if (g === null) {
+    res.send(new Error("Invalid teamId", { statusCode: 404 }))
+    next()
+  }
+
+  if (g.currentGameId) {
+    res.send(new Error("Already a game in progress", { statusCode: 409 }))
+    next()
+  }
+
+  // check to make sure the user is a member of the server
+  const matchingGuilds = await infightDB.PlayerGuild.findAndCountAll({
+    where: {
+      PlayerId: player.id,
+      GuildId: req.params.teamId
+    }
+  })
+  if (matchingGuilds.count == 0) {
+    res.send(new Error("Player not a member of that team", { statusCode: 409 }))
+    next()
+  }
+
+  // check for input size, cycle time, 
+  const cycleHours = req.body.cycleHours;
+  if (!cycleHours || isNaN(cycleHours) || cycleHours < 1 || cycleHours > 24) {
+    res.send(new Error("cycleHours is not valid", { statusCode: 400 }))
+    next()
+  }
+
+  const boardSize = req.body.boardSize;
+  if (!boardSize || isNaN(boardSize) || boardSize < 10 || boardSize > 100) {
+    res.send(new Error("boardSize is not valid", { statusCode: 400 }))
+    next()
+  }
+
+  // get all the relevant active players...?
+
+  // create the game
+  const game = infightDB.Game.build({
+    minutesPerActionDistro: cycleHours+60,
+    boardWidth: boardSize,
+    boardHeight: boardSize,
+    guildId: req.params.teamId,
+
+  });
+
+  await game.save()
+  console.log('created game ' + game.id)
+  // send some hype abouut the muster period)
+
+  res.send(game)
+})
+
+// Endpoints needed
+// Games: Create, Get
+// Moves: Create
+
+// How to run the game CRON?
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
