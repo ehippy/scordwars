d6919bf24aca2daf6dfb5b0a4b78d8466660bc43
Patrick McDavid <fusionjunky@gmail.com>
2024-12-04 18:19:45 -0700

    moved action handler up into Game

--- models/Game.js
+++ models/Game.js
@@ -254,9 +254,290 @@ module.exports = function (sequelize) {
             }
         }
 
-        async doMove() {
+        async doMove(player, action, targetX, targetY) {
             //TODO: move doMove here from app.js
+
+
+            if (this.status != 'active') {
+                throw new Exception("Game is not active")
+            }
+
+
+            // get current GamePlayer
+            var gp = null
+            for (let i = 0; i < this.GamePlayers.length; i++) {
+                const foundGp = this.GamePlayers[i];
+                if (foundGp.PlayerId == player.id) {
+                    gp = foundGp
+                }
+            }
+            if (!gp) {
+                throw new Exception("You aren't in this game")
+            }
+
+            if (gp.actions < 1 && action != 'giveHP') {
+                throw new Exception("You don't have enough AP")
+            }
+
+            if (gp.status != 'alive') {
+                throw new Exception("You're not alive.")
+            }
+
+            const guild = await this.sequelize.models.Guild.findByPk(this.GuildId)
+            if (!guild) {
+                throw new Exception("You aren't in this game")
+            }
+
+
+            if (Number.isInteger(targetX) && Number.isInteger(targetY)) {
+                if (targetX < 0 || targetX >= this.boardWidth || targetY < 0 || targetY > this.boardHeight - 1) {
+                    throw new Exception("Action is off the board")
+                }
+            }
+
+            const move = this.sequelize.models.Move.build({
+                GameId: this.id,
+                action: action,
+                targetPositionX: targetX,
+                targetPositionY: targetY,
+                actingGamePlayerId: gp.id
+            })
+
+            //for aimed actions, check range and target values
+            const currentX = gp.positionX
+            const currentY = gp.positionY
+            if (['move', 'shoot', 'giveAP', 'giveHP'].includes(action)) {
+                if (Number.isNaN(targetX) || Number.isNaN(targetY)) {
+                    throw new Exception("Target is not numeric")
+                }
+
+                let testRange = 1
+                if (action != 'move') testRange = gp.range
+
+                if (targetX < currentX - testRange || targetX > currentX + testRange || targetY < currentY - testRange || targetY > currentY + testRange) {
+                    throw new Exception("That is out of range")
+                }
+            }
+
+            //find any player in the target space
+            let targetGamePlayer = null;
+            for (let i = 0; i < this.GamePlayers.length; i++) {
+                const somePlayer = this.GamePlayers[i];
+                if (somePlayer.positionX == targetX && somePlayer.positionY == targetY) {
+                    targetGamePlayer = somePlayer
+                }
+            }
+
+
+            if (action == 'upgrade') {
+                if (gp.actions < 3) {
+                    throw new Exception("You don't have enough AP")
+                }
+                gp.range += 1
+                gp.actions -= 3
+
+                await gp.save()
+                await move.save()
+
+                this.notify("<@" + gp.PlayerId + "> 🔧 **upgraded** their range to " + gp.range + "!")
+
+                return "Upgraded!"
+            }
+
+            if (action == 'heal') {
+                if (gp.actions < 3) {
+                    throw new Exception("You don't have enough AP")
+                }
+                gp.health += 1
+                gp.actions -= 3
+
+                await gp.save()
+                await move.save()
+
+                this.notify("<@" + gp.PlayerId + "> ❤️ **healed** to **" + gp.health + "**!")
+
+                return "Upgraded!"
+            }
+
+            if (action == 'move') {
+                if (targetGamePlayer != null) {
+                    throw new Exception("A player is already in that space")
+                }
+
+                // did they collect a heart
+                let collectedHeart = false
+                for (let i = 0; i < this.boardHeartLocations.length; i++) {
+                    const heartSpot = this.boardHeartLocations[i];
+                    if (heartSpot[0] == targetX && heartSpot[1] == targetY) {
+                        gp.health += 1
+                        this.boardHeartLocations.splice(i, 1)
+                        this.changed('boardHeartLocations', true); // deep change operations in a json field aren't automatically detected by sequelize
+                        await this.save()
+                        collectedHeart = true
+                        break
+                    }
+                }
+
+                const directionDescription = this.sequelize.models.Move.describeMoveDirection([gp.positionX, gp.positionY], [targetX, targetY])
+
+                gp.positionX = targetX
+                gp.positionY = targetY
+                gp.actions -= 1
+
+                await gp.save()
+                await move.save()
+
+                const movementVerb = this.sequelize.models.Move.getRandomMovementDescriptionWithEmoji()
+                let heartPickupText = ''
+                if (collectedHeart) {
+                    heartPickupText = ' and collected a heart 💖'
+                }
+
+                this.notify(`<@${gp.PlayerId}> ${movementVerb} ${directionDescription}${heartPickupText}!`)
+
+                return "Moved!"
+            }
+
+            if (action == 'giveAP') {
+                if (targetGamePlayer == null) {
+                    return res.status(400).send("There's no player at that target to gift")
+                }
+
+                targetGamePlayer.actions += 1
+                gp.actions -= 1
+
+                await gp.save()
+                await targetGamePlayer.save()
+                move.targetGamePlayerId = targetGamePlayer.id
+                await move.save()
+
+                this.notify("<@" + gp.PlayerId + "> (" + gp.actions + " AP) 🤝 gave an AP to <@" + targetGamePlayer.PlayerId + "> (" + targetGamePlayer.actions + " AP)!")
+
+                return "Gave AP!"
+            }
+
+            if (action == 'giveHP') {
+                if (targetGamePlayer == null) {
+                    return res.status(400).send("There's no player at that target to gift")
+                }
+
+                if (gp.health < 2) {
+                    return res.status(400).send("You don't have enough health to give")
+                }
+
+                targetGamePlayer.health += 1
+                if (targetGamePlayer.health == 1) { // do a resurrection!
+                    targetGamePlayer.status = 'alive'
+                    targetGamePlayer.winPosition = null
+                    targetGamePlayer.deathTime = null
+                    await targetGamePlayer.save()
+
+                    //reshuffle the winPositions in the GamePlayers
+                    let allPlayers = await this.sequelize.models.GamePlayer.findAll({
+                        where: {
+                            GameId: this.id
+                        },
+                        order: [
+                            ['deathTime', 'ASC', 'NULLS LAST']
+                        ]
+                    })
+
+                    for (let i = 0; i < allPlayers.length; i++) {
+                        const maybeDeadPlayer = allPlayers[i];
+                        if (maybeDeadPlayer.health == 0) {
+                            maybeDeadPlayer.winPosition = allPlayers.length - i
+                            maybeDeadPlayer.save() //not awaited, might race condition 
+                        }
+                    }
+
+                }
+                gp.health -= 1
+
+                await gp.save()
+                await targetGamePlayer.save()
+                move.targetGamePlayerId = targetGamePlayer.id
+                await move.save()
+
+                if (targetGamePlayer.health == 1) {
+                    this.notify("<@" + gp.PlayerId + "> 😇 brought <@" + targetGamePlayer.PlayerId + "> back from the dead!")
+
+                } else {
+                    this.notify("<@" + gp.PlayerId + "> (" + gp.health + " HP) 💌 gave an HP to <@" + targetGamePlayer.PlayerId + "> (" + targetGamePlayer.health + " HP)!")
+                }
+
+                return "Gave HP!"
+            }
+
+            if (action == 'shoot') {
+
+                if (!targetGamePlayer) {
+                    return res.status(400).send("No player at that position")
+                }
+
+                if (targetGamePlayer.health <= 0) {
+                    return res.status(400).send("They're dead, Jim!")
+                }
+
+                targetGamePlayer.health -= 1
+                if (targetGamePlayer.health < 1) {
+                    targetGamePlayer.status = 'dead'
+                    const stolenHP = targetGamePlayer.actions
+                    gp.actions += stolenHP  // give the killer the AP of the killed
+                    targetGamePlayer.actions = 0
+                    targetGamePlayer.deathTime = new Date()
+                }
+
+                //check if game is over
+                let countAlive = 0
+                for (let i = 0; i < this.GamePlayers.length; i++) {
+                    const somePlayer = this.GamePlayers[i];
+                    if (somePlayer.status == 'alive') {
+                        countAlive++
+                    }
+                }
+                targetGamePlayer.winPosition = countAlive + 1
+                await targetGamePlayer.save()
+
+                gp.actions -= 1
+                await gp.save()
+
+                move.targetGamePlayerId = targetGamePlayer.id
+                await move.save()
+
+                let shotMsg = "<@" + gp.PlayerId + "> **💥shot💥** <@" + targetGamePlayer.PlayerId + ">, reducing their health to **" + targetGamePlayer.health + "**! 🩸"
+                if (targetGamePlayer.health == 0) {
+                    shotMsg = "### <@" + gp.PlayerId + "> **☠️ ELIMINATED ☠️** <@" + targetGamePlayer.PlayerId + ">  and stole their AP!"
+                }
+                this.notify(shotMsg)
+
+                if (countAlive == 1) {
+                    gp.winPosition = 1
+                    await gp.save()
+
+                    this.status = 'won'
+                    this.winningPlayerId = gp.id
+                    await this.save()
+
+                    guild.currentGameId = null
+                    await guild.save()
+
+                    this.notify("# 🎉 👑 <@" + gp.PlayerId + "> **_WON THE INFIGHT!!_** 🏁 🎉")
+
+                    this.sendAfterActionReport()
+
+                    setTimeout(() => {
+                        this.sequelize.models.Game.createNewGame(this.GuildId, this.boardHeight, this.boardWidth, this.minutesPerActionDistro)
+                    }, 1000 * 60 * 2) //start a new game automatically after 2 min
+                }
+
+
+                return "Shot!"
+            }
+
+            throw new Exception("Action Not implemented", action)
         }
+
+
 
         async addHeart() {
             const gamePlayers = await this.getGamePlayers()
@@ -310,7 +591,7 @@ module.exports = function (sequelize) {
             if (playerCount <= 0 || desiredDensity <= 0) {
                 throw new Exception("calculateBoardSize Player count and density must be greater than zero.");
             }
-        
+
             // Calculate the required board area for the given density
             const requiredArea = playerCount / desiredDensity;
 
